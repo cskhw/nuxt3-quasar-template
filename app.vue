f92e3beba80c8870fd3f48a7f350a6efc00c4602
김현우 <51100329+cskhw@users.noreply.github.com>
2022-11-07 16:57:50 +0900

    chore: sass 패키지 추가

--- app.vue
+++ app.vue
@@ -7,4 +7,15 @@
   </div>
 </template>
 <script setup lang="ts"></script>
-<style lang="scss"></style>
+<style lang="scss">
+@import "~/assets/stylesheets/main.scss";
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import path from "path";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
   typescript: {
     shim: false,
   },
@@ -11,6 +14,11 @@ export default defineNuxtConfig({
         // scss: {
         //   additionalData: '@use "@/assets/_colors.scss" as *;',
         // },
+      },
+    },
+    resolve: {
+      alias: {
+        "@": `${path.resolve(__dirname, "src")}`,
       },
     },
   },
