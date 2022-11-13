4dcd9c05cbfc9bb203fec7bb04d74e393d505cd6
cskhw <51100329+cskhw@users.noreply.github.com>
2022-11-13 15:03:07 +0900

    feat: 메인 홈 유저 정보 배너 UI 잡음

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,26 @@
 <template>
   <QPageContainer>
-    <QPage> home</QPage>
+    <QPage>
+      <div class="q-pa-md" style="max-width: 350px">
+        <q-expansion-item
+          label="Account settings"
+          expand-icon-class="rotate180-enter-active"
+        >
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem,
+              eius reprehenderit eos corrupti commodi magni quaerat ex numquam,
+              dolorum officiis modi facere maiores architecto suscipit iste
+              eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+      </div></QPage
+    >
   </QPageContainer>
 </template>
 <script setup lang="ts">
-import api from "@/api/api";
-import { asyncDebounce } from "@/utils/asyncDebounce";
-
-const date = new Date();
-date.setHours(date.getHours() + 3);
-const atCookie = useCookie("at", { expires: date });
-const rtCookie = useCookie("rt", { expires: date });
-
-const onClickLoginBtn = asyncDebounce(login);
-
-async function login() {
-  console.log("login");
-
-  const response = await api.auth.login({
-    email: "gusdn0828@gmail.com",
-    password: "test1234!",
-  });
-
-  atCookie.value = response.data.body.access_token;
-  rtCookie.value = response.data.body.refresh_token;
-}
+import { mdiChevronDown, mdiChevronUp } from "@mdi/js";
+const InfoState = ref(true);
 </script>
 <style lang="scss"></style>
--- plugins/quasar.client.ts
+++ plugins/quasar.client.ts
@@ -1,9 +1,11 @@
 import * as components from "quasar";
 import { QuasarPluginOptions } from "quasar";
+import mdi from "quasar/icon-set/svg-mdi-v6";
+
 export default defineNuxtPlugin(({ vueApp }) => {
   const quasarPluginOptions: QuasarPluginOptions = {
     components: components,
-    iconSet: "svg-mdi-v6",
+    iconSet: mdi,
     plugins: {},
   };
   vueApp.use(components.Quasar, quasarPluginOptions);
